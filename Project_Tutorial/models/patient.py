9066823a08e9e6df6f5303f4add3b7eced74423f
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-11 23:34:08 +0600

    create generate number

--- Project_Tutorial/models/patient.py
+++ Project_Tutorial/models/patient.py
@@ -1,4 +1,5 @@
-from odoo import api, models, fields
+from odoo import api, models, fields, _
+
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
@@ -13,9 +14,18 @@ class HospitalPatient(models.Model):
     ], required=True, default='male')
     note = fields.Text(string='Description')
     image = fields.Binary(string='Image')
+    sl = fields.Char(string='SL NO', required=True, copy=False, readonly=True, index=True,
+                     default=lambda self: _('New'))
+
+    @api.model
+    def create(self, vals):
+        if vals.get('sl', _('New')) == _('New'):
+            vals['sl'] = self.env['ir.sequence'].next_by_code('hospital.patient') or _('New')
+        res = super(HospitalPatient,self).create(vals)
+        return res
 
     @api.model
     def default_get(self, fields):
-        res = super(HospitalPatient,self).default_get(fields)
+        res = super(HospitalPatient, self).default_get(fields)
         res['note'] = 'New Patient Created'
         return res
