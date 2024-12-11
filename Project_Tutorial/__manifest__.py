b4dc9b916b6f0e1a93dd94d916494ac3c4b0f2d4
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-11 12:13:14 +0600

    create model

---
+++ Project_Tutorial/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Rakib Development Tutorials',
+    'version': '13.0.2.1.0',
+    'summary': """Odoo 13 Development Tutorials.""",
+    'sequence': '-100',
+    'description': """Odoo 13 Development Tutorials.""",
+    'category': 'Tutorials',
+    'author': 'Fahim Hasan',
+    'maintainer': 'Fahim Hasan',
+    'license': 'AGPL-3',
+    'depends': [
+
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/patient_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
