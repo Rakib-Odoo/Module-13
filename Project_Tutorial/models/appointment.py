6f16b036d9fd42033cfafbb60d99a33b6eb8ea06
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-12 12:05:08 +0600

    note default value set

--- Project_Tutorial/models/appointment.py
+++ Project_Tutorial/models/appointment.py
@@ -1,21 +1,15 @@
 from odoo import api, fields, models, _
 
+
 class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment Information'
 
-
     patient_id = fields.Many2one('hospital.patient', string='Name', required=True)
-    note = fields.Text(string='Description')
+    note = fields.Text(string='Description', default='New Appointment Created')
     date_appointment = fields.Date(string="Appointment Date", default=fields.Date.context_today, required=True)
-    date_checkup = fields.Datetime(string="Check Up Time", required=True)
+    date_checkup = fields.Datetime(string="Check Up Time", required=True, default=fields.Datetime.now)
     prescription = fields.Text(string="Prescription")
-
-    @api.model
-    def default_get(self, fields_list):
-        res = super(HospitalAppointment, self).default_get(fields_list)
-        res['note'] = 'New Appointment Created'
-        return res
 
     sl = fields.Char(string='SL NO', required=True, copy=False, readonly=True, index=True,
                      default=lambda self: _('New'))
@@ -25,4 +19,11 @@ class HospitalAppointment(models.Model):
         if vals.get('sl', _('New')) == _('New'):
             vals['sl'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or _('New')
         res = super(HospitalAppointment, self).create(vals)
-        return  res
+        return res
+
+    @api.model
+    def default_get(self, fields):
+        res = super(HospitalAppointment, self).default_get(fields)
+        if 'prescription' in fields:  # Ensure the 'prescription' field exists in the fields list
+            res['prescription'] = 'New Prescription Created'
+        return res
