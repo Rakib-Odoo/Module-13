60e31d10074e81420dd54ccb211e56322a54d45a
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-12 11:27:05 +0600

    many2one patient and appointment

--- Project_Tutorial/models/appointment.py
+++ Project_Tutorial/models/appointment.py
@@ -4,6 +4,8 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment Information'
 
+
+    patient_id = fields.Many2one('hospital.patient', string='Name', required=True)
     note = fields.Text(string='Description')
     date_appointment = fields.Date(string="Appointment Date", default=fields.Date.context_today, required=True)
     date_checkup = fields.Datetime(string="Check Up Time", required=True)
