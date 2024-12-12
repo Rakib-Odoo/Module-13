6ab6697c43d91eac4c36c1275e294fc12326745d
Rakib48-code <shahidulhuda0@gmail.com>
2024-12-12 12:33:19 +0600

    state add patient model

--- Project_Tutorial/models/patient.py
+++ Project_Tutorial/models/patient.py
@@ -16,6 +16,21 @@ class HospitalPatient(models.Model):
     image = fields.Binary(string='Image')
     sl = fields.Char(string='SL NO', required=True, copy=False, readonly=True, index=True,
                      default=lambda self: _('New'))
+    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirmed'),
+                              ('done', 'Done'), ('cancel', 'Cancelled')], default='draft',
+                             string="Status")
+
+    def action_draft(self):
+        self.state = 'draft'
+
+    def action_confirm(self):
+        self.state = 'confirm'
+
+    def action_done(self):
+        self.state = 'done'
+
+    def action_cancel(self):
+        self.state = 'cancel'
 
     @api.model
     def create(self, vals):
